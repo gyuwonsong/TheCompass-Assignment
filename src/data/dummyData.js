a822c4d4b052ae047dc999a7d4072c2bfd2b3257
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-11-06 16:28:21 +0900

    test: 더미데이터 추가

---
+++ src/data/dummyData.js
@@ -0,0 +1,95 @@
+export const projects = [
+    {
+        id: "1",
+        title: "프로젝트 A",
+        description: "첫 번째 프로젝트",
+        tasks: ["1"]
+    },
+    {
+        id: "2",
+        title: "프로젝트 B",
+        description: "두 번째 프로젝트",
+        tasks: ["2"]
+    },
+    {
+        id: "3",
+        title: "프로젝트 C",
+        description: "세 번째 프로젝트",
+        tasks: ["3"]
+    }
+];
+
+export const tasks = [
+    {
+        pjId: "1",
+        id: "1",
+        title: "태스크 1",
+        description: "첫 번째 프로젝트의 첫 번째 태스크",
+        priority: "high",
+        dueDate: "2024-11-10",
+        status: "in-progress"
+    },
+    {
+        pjId: "1",
+        id: "2",
+        title: "태스크 2",
+        description: "첫 번째 프로젝트의 두 번째 태스크",
+        priority: "low",
+        dueDate: "2024-11-20",
+        status: "done"
+    },
+    {
+        pjId: "2",
+        id: "3",
+        title: "태스크 3",
+        description: "두 번째 프로젝트의 첫 번째 태스크",
+        priority: "medium",
+        dueDate: "2024-11-15",
+        status: "in-progress"
+    },
+    {
+        pjId: "3",
+        id: "4",
+        title: "태스크 1",
+        description: "세 번째 프로젝트의 첫 번째 태스크",
+        priority: "high",
+        dueDate: "2024-11-06",
+        status: "in-progress"
+    },
+    {
+        pjId: "3",
+        id: "5",
+        title: "태스크 2",
+        description: "세 번째 프로젝트의 두 번째 태스크",
+        priority: "medium",
+        dueDate: "2024-11-08",
+        status: "not-started"
+    },
+    {
+        pjId: "3",
+        id: "6",
+        title: "태스크 3",
+        description: "세 번째 프로젝트의 세 번째 태스크",
+        priority: "low",
+        dueDate: "2024-11-13",
+        status: "in-progress"
+    },
+    {
+        pjId: "3",
+        id: "7",
+        title: "태스크 4",
+        description: "세 번째 프로젝트의 네 번째 태스크",
+        priority: "medium",
+        dueDate: "2024-11-01",
+        status: "done"
+    },
+    {
+        pjId: "3",
+        id: "8",
+        title: "태스크 5",
+        description: "세 번째 프로젝트의 다섯 번째 태스크",
+        priority: "high",
+        dueDate: "2024-11-30",
+        status: "not-started"
+    }
+];
